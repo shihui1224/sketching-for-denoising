89fa9f3549efae8d4927e29aec2c115a6668e6c2
shi <hui.shi@math.u-bordeaux.fr>
2021-11-15 16:14:33 +0100

    add licence

--- sketching_m/sk_learn.m
+++ sketching_m/sk_learn.m
@@ -1,3 +1,19 @@
+% A script to learn a GMM prior given the empirical sketch (sk_file.mat)
+% Copyright (C) 2021 Shi Hui
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Affero General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Affero General Public License for more details.
+
+% You should have received a copy of the GNU Affero General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 op = 1;
 r = 20; k = 20;
 skw_file = 'sk_file.mat';
